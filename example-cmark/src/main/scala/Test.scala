4726b3fcbfd3b27986cd572fdc8bdfb08d77fac4
Anton Sviridov <keynmol@gmail.com>
2022-05-24 11:05:30 +0100

    Add cmark

---
+++ example-cmark/src/main/scala/Test.scala
@@ -0,0 +1,50 @@
+import cmark.types.*
+import cmark.functions.*
+
+import scala.scalanative.unsafe.*
+import scala.scalanative.libc.*
+import scala.scalanative.libc
+
+@main def hello =
+  Zone { implicit z =>
+    val str = toCString(
+      """
+    |# Hello!
+    |
+    |*stuff*
+    |## yo
+    |```scala
+    |val x = 1
+    |```
+    |### hi
+    """.stripMargin
+    )
+
+    val node = cmark_parse_document(str, libc.string.strlen(str), 0)
+
+    def iterateNodes(node: Ptr[cmark_node]): Unit =
+      def go(nd: Ptr[cmark_node]): Unit =
+        val iter = cmark_iter_new(nd)
+        var ev_type: cmark_event_type = cmark_event_type.CMARK_EVENT_NONE
+
+        while {
+          ev_type = cmark_iter_next(iter); ev_type
+        } != cmark_event_type.CMARK_EVENT_DONE do
+          val cur = cmark_iter_get_node(iter)
+
+          stdio.printf(
+            c"Node: %s, ev_type: %d, address: %p \n",
+            cmark_node_get_type_string(cur),
+            ev_type,
+            cur
+          )
+
+        end while
+
+      end go
+
+      go(node)
+    end iterateNodes
+
+    iterateNodes(node)
+  }
