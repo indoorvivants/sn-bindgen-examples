bfc41424abc81eeb2f0f8a3e266892683c43f5ab
Anton Sviridov <keynmol@gmail.com>
2022-09-09 16:48:13 +0100

    Print s2n error

--- example-s2n/src/main/scala/Test.scala
+++ example-s2n/src/main/scala/Test.scala
@@ -1,5 +1,9 @@
 import s2n.functions.*
 import s2n.types.*
 
-@main def hello_s2n = 
-  assert(s2n_init() == 0)
+import scalanative.unsafe.*
+
+@main def hello_s2n =
+  val init = s2n_init()
+  if init == 0 then println("Ah ok amigo")
+  else println(fromCString(s2n_strerror(init, c"EN")))
